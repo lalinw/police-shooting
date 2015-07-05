97cf2900328c4f2d2bea98ce6897384124bbbe1c
Irene W <i.lalinw@gmail.com>
2015-07-05 00:22:22 -0700

    basic changes added

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
-// Function to draw your map
+ // Function to draw your map
 var drawMap() = function() {
 
   // Create map and set viewd
- 
+	 
 
   // Create an tile layer variable using the appropriate url
 
@@ -10,7 +10,13 @@ var drawMap() = function() {
  
 
   // Execute your function to get data
- 
+
+  var map = L.map('map-container');
+  map.SetView([lat, longi], 12);
+
+  L.tileLayer('http://{s}.url/{z}/{x}/{y}.png').addTo(map);
+
+  getData();
 }
 
 // Function for getting data
@@ -20,13 +26,28 @@ var getData = function() {
 
 
   // When your request is successful, call your customBuild function
+  var data;
+  $.ajax({
+  	url:'https://data.seattle.gov/resource/7ais-f98f.json',
+  	type: 'get',
+  	success:function(d) {
+  		data = d;
 
+  		customBuild();
+  	},
+  	dataType: 'json'
+  });
 }
 
 // Do something creative with the data here!  
 var customBuild = function() {
+	data.map(function(d) {
+		var circle = new L.circle([d.latitude, d.longitude], 200, {
+			color:'red',
+			opacity: 0.5
+		}).addTo(map);
+	});
 
-  
 }
 
 
