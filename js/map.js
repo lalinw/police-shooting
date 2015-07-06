cc7d05f17d3f5763fcafe1d7e5d6f90a0c4c8778
Irene W <i.lalinw@gmail.com>
2015-07-06 13:57:25 -0700

    get it to work

--- js/map.js
+++ js/map.js
@@ -11,7 +11,6 @@ var drawMap = function() {
 
 // Function for getting data
 var getData = function(theMap) {
-
   // Execute an AJAX request to get the data in data/response.js
   // When your request is successful, call your customBuild function
   var data;
@@ -19,7 +18,9 @@ var getData = function(theMap) {
   	url:'data/response.json',
   	type:'get',
     async: false,
-  	success: function(dem) {data = dem;},
+  	success: function(dem) {
+      data = dem;
+    },
   	dataType:'json'
   });
   customBuild(theMap, data);  
@@ -28,13 +29,26 @@ var getData = function(theMap) {
 
 // Do something creative with the data here!  
 var customBuild = function(theMap, data) {
-		data.map(function(dis) {
-    var cirMa = L.circleMarker([dis.lat, dis.lng],
-      {radius: 5, 
-      color: 'red', 
+	data.map(function(dis) {
+    
+    var clr;
+    if (dis['Hit or Killed?'] == 'Hit') {
+      clr = 'blue';
+    } else { //Killed is red
+      clr = 'red';
+    }
+
+    var cirMa = L.circleMarker(
+      [dis.lat, dis.lng],
+      {radius: 10, 
+      color: clr, 
       opacity: 0.7, 
       stroke: false
     }).addTo(theMap);
+    var text = "<b>Location:</b> " + dis.City + ", " + dis.State.substring(0,2) + "<br/>" +
+      "<b>Date:</b> " + dis.Timestamp + "<br/>" +
+      "<b>Victim:</b> " + dis.Race + " " + dis["Victim's Gender"] + ", " + dis["Victim's Age"] + " yrs";
+    cirMa.bindPopup(text);
   });
 
 }
